7e4125ce16273065618a40b2605cab7795f09b58
Veankata Krishnan S M <veankatakrishnan@gmail.com>
2026-05-14 11:02:44 +0530

    Time: 3 ms (59.73%) | Memory: 44.7 MB (48.16%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int l = 0, r = s.length() - 1;
+        while(l < r){
+            while(l < r && !Character.isLetterOrDigit(s.charAt(l))){
+                l++;
+            }
+            while(l < r && !Character.isLetterOrDigit(s.charAt(r))){
+                r--;
+            }
+            if(s.charAt(l) != s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
