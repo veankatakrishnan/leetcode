4e672f8c5cfb561a8819a7eada3e2ecf3382d843
Veankata Krishnan S M <veankatakrishnan@gmail.com>
2026-05-14 11:41:17 +0530

    Time: 1 ms (29.72%) | Memory: 42.9 MB (82.67%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String trimmed = s.trim();
+        String[] words = trimmed.split(" ");
+        return words[words.length - 1].length();
+    }
+}
