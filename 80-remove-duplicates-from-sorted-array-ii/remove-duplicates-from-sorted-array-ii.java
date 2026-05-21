dd6db350123fafbc133a50eef87fc610c0b27dbf
Veankata Krishnan S M <veankatakrishnan@gmail.com>
2026-05-21 22:41:58 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (83.20%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int index =  2;
+        for(int i = 2; i < nums.length; i++){
+            if(nums[i] != nums[index - 2]){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
