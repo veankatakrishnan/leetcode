4e230fed09ccec09494bd02a212a0bb9b23fff3c
Veankata Krishnan S M <veankatakrishnan@gmail.com>
2026-05-21 22:56:39 +0530

    Time: 3 ms (74.26%) | Memory: 52.4 MB (64.36%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -6,16 +6,20 @@ class Solution {
         int candidate1 = 0, candidate2 = 0;
         int count1 = 0, count2 = 0;
         for(int i = 0; i < nums.length; i++){
-            if(count1 == 0 && candidate2 != nums[i]){
+            if(count1 == 0 && nums[i] != candidate2){
                 count1++;
                 candidate1 = nums[i];
             }
-            else if(count2 == 0 && candidate1 != nums[i]){
+            else if(count2 == 0 && nums[i] != candidate1){
                 count2++;
                 candidate2 = nums[i];
             }
-            else if(candidate1 == nums[i]) count1++;
-            else if(candidate2 == nums[i]) count2++;
+            else if(nums[i] == candidate1){
+                count1++;
+            }
+            else if(nums[i] == candidate2){
+                count2++;
+            }
             else{
                 count1--;
                 count2--;
@@ -23,12 +27,20 @@ class Solution {
         }
         count1 = 0;
         count2 = 0;
-        for(int num: nums){
-            if(num == candidate1) count1++;
-            else if(num == candidate2) count2++;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == candidate1){
+                count1++;
+            }
+            else if(nums[i] == candidate2){
+                count2++;
+            }
         }
-        if(count1 > nums.length / 3) majority.add(candidate1);
-        if(count2 > nums.length / 3) majority.add(candidate2);
+        if((nums.length / 3) < count1){
+            majority.add(candidate1);
+        }
+        if((nums.length / 3) < count2){
+            majority.add(candidate2);
+        }
         return majority;
     }
 }
