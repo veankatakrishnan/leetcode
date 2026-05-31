34dca575620ee5b099d02c90a1e2bca964060ca5
Veankata Krishnan S M <veankatakrishnan@gmail.com>
2026-05-31 15:42:40 +0530

    Time: 3 ms (91.86%) | Memory: 46.5 MB (23.63%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -5,15 +5,6 @@ class Solution {
         char[] t1 = t.toCharArray();
         Arrays.sort(s1);
         Arrays.sort(t1);
-        if(s1.length != t1.length){
-            return false;
-        }
-            
-        for(int i = 0; i < t1.length; i++){
-            if(s1[i] != t1[i]){
-                return false;
-            }
-        }
-        return true;
+        return Arrays.equals(s1, t1);
     }
 }
