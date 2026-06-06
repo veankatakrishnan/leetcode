20281a99be9be18e6def82a35933f4c732dc8647
Veankata Krishnan S M <veankatakrishnan@gmail.com>
2026-06-06 10:39:39 +0530

    Time: 1 ms (98.46%) | Memory: 47.7 MB (42.28%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,15 +1,14 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        int col1 = 1;
         int n = matrix.length;
         int m = matrix[0].length;
-        int col1 = 1;
+
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(matrix[i][j] == 0){
                     matrix[i][0] = 0;
-                    if(j != 0){
-                        matrix[0][j] = 0;
-                    }
+                    if(j != 0) matrix[0][j] = 0;
                     else col1 = 0;
                 }
             }
@@ -20,13 +19,18 @@ class Solution {
                 if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
             }
         }
-
         if(matrix[0][0] == 0){
-            for(int j = 0; j < m; j++) matrix[0][j] = 0;
+            for(int j = 0; j < m; j++){
+                matrix[0][j] = 0;
+            }
         }
-        
+
         if(col1 == 0){
-            for(int i = 0; i < n; i++) matrix[i][0] = 0;
+            for(int i = 0; i < n; i++){
+                matrix[i][0] = 0;
+            }
         }
+
+        
     }
 }
