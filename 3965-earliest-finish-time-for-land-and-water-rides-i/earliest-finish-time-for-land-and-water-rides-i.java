7b4675bf2ec9cebeb947ec6bb4bab3eed8444389
Veankata Krishnan S M <veankatakrishnan@gmail.com>
2026-06-11 12:20:50 +0530

    Time: 1 ms (100.00%) | Memory: 47 MB (40.44%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
@@ -1,23 +1,23 @@
 class Solution {
     public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
-        int land = landStartTime.length, water = waterStartTime.length; 
-        int minimumTime = Integer.MAX_VALUE;
-        for(int i = 0; i < land; i++){
-            int landFinishTime = landStartTime[i] + landDuration[i];
-            for(int j = 0; j < water; j++){
-                int waterBegin = Math.max(waterStartTime[j], landFinishTime);
-                int totalFinish = waterBegin + waterDuration[j];
-                minimumTime = Math.min(minimumTime, totalFinish);
-            }
+        int landThenWater = minimumFinishTime(landStartTime, landDuration, waterStartTime, waterDuration);
+        int waterThenLand = minimumFinishTime(waterStartTime, waterDuration, landStartTime, landDuration);
+        return (Math.min(landThenWater, waterThenLand));
+    }
+
+    public int minimumFinishTime(int[] firstStartTime, int[] firstDuration, int[]secondStartTime, int[]secondDuration){
+        int firstMinimum = Integer.MAX_VALUE;
+        for(int i = 0; i < firstStartTime.length; i++){
+            int minimum = firstStartTime[i] + firstDuration[i];
+            firstMinimum = Math.min(minimum, firstMinimum);
         }
-        for(int j = 0; j < water; j++){
-            int waterFinishTime = waterStartTime[j] + waterDuration[j];
-            for(int i = 0; i < land; i++){
-                int landBegin = Math.max(waterFinishTime, landStartTime[i]);
-                int totalFinish = landBegin + landDuration[i];
-                minimumTime = Math.min(minimumTime, totalFinish);
-            }
+
+        int secondMinimum = Integer.MAX_VALUE;
+        for(int i = 0; i < secondDuration.length; i++){
+            int secondBegin = Math.max(firstMinimum, secondStartTime[i]);
+            int minimum = secondDuration[i] + secondBegin;
+            secondMinimum = Math.min(minimum, secondMinimum);
         }
-        return minimumTime;
+        return (secondMinimum);
     }
 }
