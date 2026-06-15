20941d64fb766d1bcd3535897025a3caa53c58a1
Veankata Krishnan S M <veankatakrishnan@gmail.com>
2026-06-15 12:54:51 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (88.65%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.java
+++ 566-reshape-the-matrix/reshape-the-matrix.java
@@ -1,25 +1,12 @@
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-        int n = mat.length;
-        int m = mat[0].length;
-        if(n*m != r*c) return mat;
-
         int[][] reshaped = new int[r][c];
-        int[] temp = new int[r*c];
-        int index = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                temp[index] = mat[i][j];
-                index++;
-            }
-        }
-
-        index = 0;
-        for(int i = 0; i < r; i++){
-            for(int j = 0; j < c; j++){
-                reshaped[i][j] = temp[index];
-                index++;
-            }
+        int m = mat.length;
+        int n = mat[0].length;
+        if(m*n != r*c) return mat;
+        int index = r * c;
+        for(int i = 0; i < index; i++){
+            reshaped[i / c][i % c] = mat[i / n][i % n];
         }
         return reshaped;
     }
