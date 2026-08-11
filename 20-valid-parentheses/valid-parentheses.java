60b6367ae2ddfa156ff17ba37439bd606478d9d8
Veankata Krishnan S M <veankatakrishnan@gmail.com>
2026-08-11 06:44:36 +0530

    Time: 3 ms (86.07%) | Memory: 43.3 MB (41.35%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }
+            else{
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+                if((top == '(' && ch == ')') || (top == '{' && ch =='}') || (top == '[' && ch ==']')){
+                    continue;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
